6d7ccb97965ffcc621a012a6af588fef096562d3
Carl-Johan Landin <carl-johan.landin@evidente.se>
2024-04-18 03:05:58 +0200

    Add custom Kconfig symbol

--- src/main.c
+++ src/main.c
@@ -7,6 +7,11 @@
 
 int main(void)
 {
+
+#ifdef CONFIG_USR_FUN
+	usr_fun();
+#endif
+
 	while (1) {
 		printk("Hello, World!\n");
 		k_sleep(K_MSEC(1000));
---
+++ src/usr_fun.c
@@ -0,0 +1,7 @@
+#include "usr_fun.h"
+#include <zephyr/kernel.h>
+
+void usr_fun(void)
+{
+    printk("Message in a user function.\n");
+}
